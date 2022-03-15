669d9843f4b8ab3b082a023eaa08a7a7b6d8086c
eduardo.velasques <eduardo.velasques@sitana.pt>
2022-03-15 19:03:39 +0000

    Social links added in the footer.

--- website/src/base/Footer/index.js
+++ website/src/base/Footer/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Layout, Row, Col } from 'antd';
 import { PhoneOutlined, HomeOutlined, MailOutlined } from '@ant-design/icons';
-import Dictionary from '../../components/Dictionary';
+import { FaHome, FaPhone, FaEnvelope, FaYoutube, FaGithub, FaFacebook, FaLinkedin, FaTwitter, FaInstagram, FaDiscord } from 'react-icons/fa';
+import { RiOpenSourceFill } from 'react-icons/ri';
+import Configuration from '../../components/Configuration';
 
 import './index.less';
 
@@ -21,19 +23,42 @@ function BaseFooter() {
             <Col><HomeOutlined /></Col>
             <Col>&nbsp;</Col>
             <Col>
-              <address><Dictionary entry="footer-address"/></address>
+              <address><Configuration parameter="footer-address" multilines/></address>
             </Col>
           </Row>
           <Row>
             <Col>
-              <p><PhoneOutlined /> <Dictionary entry="footer-phone" oneLine/></p>
+              <p><PhoneOutlined /> <Configuration parameter="footer-phone"/></p>
             </Col>
           </Row>
           <Row>
             <Col>
-              <p><MailOutlined /> <Dictionary entry="footer-email" oneLine/></p>
+              <p><MailOutlined /> <Configuration parameter="footer-email"/></p>
             </Col>
           </Row>
+          <p className="social-links">
+            <a href="https://github.com/netuno-org" className="share-icons">
+              <FaGithub />
+            </a>
+            <a href="https://www.youtube.com/channel/UCYY1Nz6T2NJtP29vba2fqkg" className="share-icons">
+              <FaYoutube />
+            </a>
+            <a href="https://twitter.com/netuno_org" className="share-icons">
+              <FaTwitter />
+            </a>
+            <a href="https://www.linkedin.com/company/netuno-org/" className="share-icons">
+              <FaLinkedin />
+            </a>
+            <a href="https://discord.gg/4sfXG6YWFu" className="share-icons">
+              <FaDiscord />
+            </a>
+            <a href="https://www.facebook.com/netuno.org/" className="share-icons">
+              <FaFacebook />
+            </a>
+            <a href="https://www.instagram.com/netuno_org/" className="share-icons">
+              <FaInstagram />
+            </a>
+          </p>
         </Col>
       </Row>
     </Footer>
