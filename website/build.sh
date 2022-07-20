a2bb86364e1691dba3e3558db5595407346cf2d6
Eduardo Velasques <eduveks@gmail.com>
2022-07-20 16:54:08 +0100

    Update build.sh

--- website/build.sh
+++ website/build.sh
@@ -2,6 +2,7 @@
 
 if [ -f "build/cluarData.js" ]; then
     mv build/cluarData.js .cluarData.js
+    mv build/sitemap.xml .sitemap.xml
 fi
 
 if [ -d "build/images" ]; then
@@ -14,4 +15,5 @@ npm run build
 
 if [ -f ".cluarData.js" ]; then
     mv .cluarData.js build/cluarData.js
+    mv .sitemap.xml build/sitemap.xml
 fi
