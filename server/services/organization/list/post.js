f7379cd17bc1680d0df3992750856ab134d71187
JailtonAraujo <jailtonraneimoney@gmail.com>
2025-04-12 23:47:52 -0300

    modify organizatin query list to use recursive operation

--- server/services/organization/list/post.js
+++ server/services/organization/list/post.js
@@ -15,7 +15,7 @@ if (pagination) {
 
 if (filters) {
     const name = filters.has('name') && filters.getString('name');
-    
+
     if (name) {
         queryWhere += `
             AND org.name like ?
@@ -24,7 +24,7 @@ if (filters) {
     }
 
     const code = filters.has('code') && filters.getString('code');
-    
+
     if (code) {
         queryWhere += `
             AND org.code like ?
@@ -50,19 +50,50 @@ if (filters) {
     }
 }
 
+const dbPeople = _db.queryFirst(`
+    SELECT id FROM people WHERE people_user_id = ? 
+`, _user.id());
+
 const dbOrganizations = _db.query(`
-    SELECT
-        org.active AS org_active,
-        org.uid AS org_uid, 
-        org.code AS org_code,
-        org.name AS org_name,
+    WITH RECURSIVE user_orgs AS (
+        SELECT 
+            org.name, 
+            org.id, 
+            org.parent_id,
+            org.code,
+            org.uid,
+            org.active
+        FROM 
+            organization org
+        INNER JOIN 
+            organization_people op ON org.id = op.organization_id
+        WHERE 1 = 1 
+            AND op.people_id = ${dbPeople.getInt("id")}
+            AND op.user_group_id = (SELECT id FROM user_group WHERE code = 'administrator')
+        UNION
+        SELECT 
+            org.name, 
+            org.id, 
+            org.parent_id,
+            org.code,
+            org.uid,
+            org.active
+        FROM 
+            organization org
+        INNER JOIN user_orgs uo ON org.parent_id = uo.id
+    )
+    SELECT 
+        user_orgs.name AS org_name,
+        user_orgs.code AS org_code,
+        user_orgs.uid AS org_uid,
+        user_orgs.active AS org_active,
         parent.uid AS parent_uid,
         parent.code AS parent_code,
         parent.name AS parent_name
     FROM 
-        organization org
+        user_orgs
     LEFT JOIN 
-        organization parent on org.parent_id = parent.id
+        organization parent ON user_orgs.parent_id = parent.id
     WHERE 1 = 1
         ${queryWhere}
     LIMIT ${page.size()} OFFSET ${page.offset()}     
@@ -80,23 +111,50 @@ for (const dbOrganization of dbOrganizations) {
     if (dbOrganization.has('parent_uid')) {
         organization.set("parent",
             _val.map()
-            .set('uid', dbOrganization.getString('parent_uid'))
-            .set('name', dbOrganization.getString('parent_name'))
-            .set('code', dbOrganization.getString('parent_code'))
+                .set('uid', dbOrganization.getString('parent_uid'))
+                .set('name', dbOrganization.getString('parent_name'))
+                .set('code', dbOrganization.getString('parent_code'))
         )
     }
     organizations.add(organization);
 }
 
 const dbOrganizationTotal = _db.queryFirst(`
-     SELECT
-        COUNT(1) as total
+    WITH RECURSIVE user_orgs AS (
+        SELECT 
+            org.name, 
+            org.id, 
+            org.parent_id,
+            org.code,
+            org.uid,
+            org.active
+        FROM 
+            organization org
+        INNER JOIN 
+            organization_people op ON org.id = op.organization_id
+        WHERE 1 = 1 
+            AND op.people_id = ${dbPeople.getInt("id")}
+            AND op.user_group_id = (SELECT id FROM user_group WHERE code = 'administrator')
+        UNION
+        SELECT 
+            org.name, 
+            org.id, 
+            org.parent_id,
+            org.code,
+            org.uid,
+            org.active
+        FROM 
+            organization org
+        INNER JOIN user_orgs uo ON org.parent_id = uo.id
+    )
+    SELECT 
+        COUNT(1) AS "total"
     FROM 
-        organization org
+        user_orgs
     LEFT JOIN 
-        organization parent on org.parent_id = parent.id
+        organization parent ON user_orgs.parent_id = parent.id
     WHERE 1 = 1
-        ${queryWhere}  
+        ${queryWhere}
 `, queryParams);
 
 _header.status(201);
