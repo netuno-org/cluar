3979cdea692fa2422ff33c7834918879b269168b
JailtonAraujo <jailtonraneimoney@gmail.com>
2025-04-17 16:57:16 -0300

    fix list organization member query to group by org and people

--- server/services/organization/member/list/post.js
+++ server/services/organization/member/list/post.js
@@ -32,13 +32,13 @@ if (filters) {
         queryParams.add(`%${peopleName}%`)
     }
 
-    const groupName = filters.has("group_name") && filters.getString("group_name");
+    const groupCodes = filters.has("group_codes") && filters.getList("group_codes");
 
-    if (groupName) {
+    if (groupCodes.length > 0) {
         queryWhere += `
-            AND user_group.name like ?
+            AND user_group.code IN (${groupCodes.map(() => '?').join(", ")})
         `
-        queryParams.add(`%${groupName}%`)
+        queryParams.addAll(groupCodes)
     }
 }
 
@@ -74,27 +74,24 @@ const dbMembers = _db.query(`
             organization org
         INNER JOIN user_orgs uo ON org.parent_id = uo.id
     )
-    SELECT
+    SELECT DISTINCT ON (user_orgs.uid, people.uid)
         organization_people.uid AS organization_people_uid,
         organization_people.active AS organization_people_active, 
+        user_orgs.id AS user_orgs_id,
         user_orgs.name AS org_name,
         user_orgs.code AS org_code,
         user_orgs.uid AS org_uid,
+        people.id AS people_id,
         people.uid AS people_uid,
-        people.name AS people_name,
-        user_group.name AS group_name,
-        user_group.code AS group_code,
-        user_group.uid AS group_uid
+        people.name AS people_name
     FROM 
         user_orgs
-    LEFT JOIN 
-        organization parent ON user_orgs.parent_id = parent.id
 	INNER JOIN 
         organization_people ON organization_people.organization_id = user_orgs.id
 	INNER JOIN
 		people ON people.id = organization_people.people_id
-	INNER JOIN
-		user_group ON user_group.id = organization_people.user_group_id
+    INNER JOIN 
+        user_group ON user_group.id = organization_people.user_group_id
     WHERE 1 = 1
         ${queryWhere}
     LIMIT ${page.size()} OFFSET ${page.offset()}  
@@ -129,17 +126,15 @@ const dbMembersTotal = _db.queryFirst(`
         INNER JOIN user_orgs uo ON org.parent_id = uo.id
     )
     SELECT
-        COUNT(1) AS "total"
+        COUNT(DISTINCT (user_orgs.uid, people.uid)) AS "total"
     FROM 
         user_orgs
-    LEFT JOIN 
-        organization parent ON user_orgs.parent_id = parent.id
 	INNER JOIN 
         organization_people ON organization_people.organization_id = user_orgs.id
 	INNER JOIN
 		people ON people.id = organization_people.people_id
-	INNER JOIN
-		user_group ON user_group.id = organization_people.user_group_id
+    INNER JOIN 
+        user_group ON user_group.id = organization_people.user_group_id
     WHERE 1 = 1
         ${queryWhere}
 `, queryParams);
@@ -147,6 +142,17 @@ const dbMembersTotal = _db.queryFirst(`
 const members = _val.list();
 
 for (const dbMember of dbMembers) {
+    const dbGroups = _db.form('user_group')
+        .link(
+            'organization_people',
+            _db.where()
+                .and('people_id').equals(dbMember.getInt("people_id"))
+                .and('organization_id').equals(dbMember.getInt("user_orgs_id"))
+        )
+        .get("user_group.name")
+        .get("user_group.code")
+        .all()
+
     members.add(
         _val.map()
             .set('uid', dbMember.getString("organization_people_uid"))
@@ -160,11 +166,7 @@ for (const dbMember of dbMembers) {
                 .set('uid', dbMember.getString("people_uid"))
                 .set('name', dbMember.getString("people_name"))
             )
-            .set('group', _val.map()
-                .set('uid', dbMember.getString("group_uid"))
-                .set('name', dbMember.getString("group_name"))
-                .set('code', dbMember.getString("group_code"))
-            )
+            .set('groups', dbGroups)
     )
 }
 
