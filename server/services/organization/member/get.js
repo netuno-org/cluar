603b2b8169b3b09682eb6efbbd179438191060aa
JailtonAraujo <jailtonraneimoney@gmail.com>
2025-04-17 17:21:01 -0300

    fix member get service and member open_api schema

--- server/services/organization/member/get.js
+++ server/services/organization/member/get.js
@@ -33,27 +33,24 @@ const dbMember = _db.queryFirst(`
             organization org
         INNER JOIN user_orgs uo ON org.parent_id = uo.id
     )
-    SELECT
+    SELECT DISTINCT ON (user_orgs.uid, people.uid)
         organization_people.uid AS organization_people_uid,
         organization_people.active AS organization_people_active, 
+        user_orgs.id AS user_orgs_id,
         user_orgs.name AS org_name,
         user_orgs.code AS org_code,
         user_orgs.uid AS org_uid,
+        people.id AS people_id,
         people.uid AS people_uid,
-        people.name AS people_name,
-        user_group.name AS group_name,
-        user_group.code AS group_code,
-        user_group.uid AS group_uid
+        people.name AS people_name
     FROM 
         user_orgs
-    LEFT JOIN 
-        organization parent ON user_orgs.parent_id = parent.id
 	INNER JOIN 
         organization_people ON organization_people.organization_id = user_orgs.id
 	INNER JOIN
 		people ON people.id = organization_people.people_id
-	INNER JOIN
-		user_group ON user_group.id = organization_people.user_group_id
+    INNER JOIN 
+        user_group ON user_group.id = organization_people.user_group_id
     WHERE 1 = 1
         AND organization_people.uid = ?::uuid
 `, uid);
@@ -68,6 +65,17 @@ if (!dbMember) {
     );
     _exec.stop();
 }
+
+const dbGroups = _db.form('user_group')
+.link(
+    'organization_people',
+    _db.where()
+        .and('people_id').equals(dbMember.getInt("people_id"))
+        .and('organization_id').equals(dbMember.getInt("user_orgs_id"))
+)
+.get("user_group.name")
+.get("user_group.code")
+.all()
 
 _out.json(
     _val.map()
@@ -85,10 +93,6 @@ _out.json(
                     .set('uid', dbMember.getString("people_uid"))
                     .set('name', dbMember.getString("people_name"))
                 )
-                .set('group', _val.map()
-                    .set('uid', dbMember.getString("group_uid"))
-                    .set('name', dbMember.getString("group_name"))
-                    .set('code', dbMember.getString("group_code"))
-                )
+                .set('groups', dbGroups)
         )
 )
