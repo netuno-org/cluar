ea26ba3feba596f5298a67dc9bf1ba56577521f0
JailtonAraujo <jailtonraneimoney@gmail.com>
2025-04-28 14:46:29 -0300

    create core method to get organization related to user

--- server/core/utils/user.js
+++ server/core/utils/user.js
@@ -40,7 +40,46 @@ const isUserAuthorizedInOrganization = (params) => {
 }
 
 const getUserOrganizations = () => {
+    const dbPeople = _db.queryFirst(`SELECT id FROM people WHERE people_user_id = ?`, _user.id());
 
+    const dbOrganizations = _db.query(`
+        WITH RECURSIVE user_orgs AS (
+            SELECT 
+                org.name, 
+                org.id, 
+                org.parent_id,
+                org.code,
+                org.uid,
+                org.active
+            FROM 
+                organization org
+            INNER JOIN 
+                organization_people op ON org.id = op.organization_id
+            WHERE 1 = 1 
+                AND op.people_id = ${dbPeople.getInt("id")}
+                AND op.user_group_id = (SELECT id FROM user_group WHERE code = 'administrator')
+                AND op.active = true
+            UNION
+            SELECT 
+                org.name, 
+                org.id, 
+                org.parent_id,
+                org.code,
+                org.uid,
+                org.active
+            FROM 
+                organization org
+            INNER JOIN user_orgs uo ON org.parent_id = uo.id
+        )
+        SELECT 
+            user_orgs.name,
+            user_orgs.code,
+            user_orgs.uid,
+            user_orgs.uid
+        FROM 
+            user_orgs
+        WHERE 1 = 1   
+        `);
 }
 
 const getUserGroupEachOrganization = () => {
