91ce0aabc2390975dce40e8417088cfba8cbedf3
jcardoso17 <jpcardoso97@gmail.com>
2025-05-28 17:27:02 +0100

    Update setup

---
+++ server/setup/019-page_version.js
@@ -0,0 +1,255 @@
+
+
+// -----------------------------------------------------------
+// 
+// PAGE_VERSION
+// 
+// -----------------------------------------------------------
+// 
+// CODE GENERATED AUTOMATICALLY
+// 
+
+
+_db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "9529dfcc-9a15-45ab-9f30-fee4bc75d154")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 1)
+      .set("status_id", "e24a0cdf-c860-48f2-b597-689f165fd110")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "2a66c202-1b77-4c0e-ac55-945fa8af384f")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 2)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "62abbece-2708-4f65-b495-b26077057b10")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 3)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "5c79dc39-8793-4189-96ed-764ad1c5665a")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 4)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "7cdac2f5-83fe-46cf-9395-16c08f334f10")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 5)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "d01620eb-86cc-4e5c-9241-19ff21953415")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 6)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "7bec04dc-26fc-45c1-8682-d775b32f599f")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 7)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "8917aca4-9ac8-4ec4-a568-746ea253ecc3")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 8)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "6a008481-b812-44f0-9587-d4d32f78a16b")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 9)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "26b3a1d4-ef07-409c-93cb-a7e9e6da0889")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 10)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "e3eeea55-b47c-4cfa-9af4-13b872c4af72")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 11)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "6f136629-0306-46e3-b72c-2e7595f7626b")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 12)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "d50981be-64d2-46d6-9eee-bde209314865")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 13)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "acf837a1-b7a7-4264-b07e-683e96fad806")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 14)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "12b264e1-9c28-423f-a2ea-6131cedc6010")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 15)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "c60ccc5d-a4b8-440b-bbd7-da7d7e234cf6")
+      .set("page_id", "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9")
+      .set("version", 16)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "eb14d6ee-db83-434c-879e-20e0825cff21")
+      .set("page_id", "5002a742-e092-4c0b-8536-546bd1319c7f")
+      .set("version", 1)
+      .set("status_id", "e24a0cdf-c860-48f2-b597-689f165fd110")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "3944a818-e5e4-4fc3-86fb-18912a8b3bb0")
+      .set("page_id", "187ff9e4-50fa-4824-a46a-3c40420942d3")
+      .set("version", 1)
+      .set("status_id", "e24a0cdf-c860-48f2-b597-689f165fd110")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "3969b5af-54da-4294-a9e0-8b9f6a07c027")
+      .set("page_id", "0be9036a-a2c4-4eca-9020-2f46bb2fa5dc")
+      .set("version", 1)
+      .set("status_id", "e24a0cdf-c860-48f2-b597-689f165fd110")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "ecfb95a7-fb6a-49ab-82f2-e12492df2816")
+      .set("page_id", "4e2312c1-0a9a-438f-ad59-82e1fbea89a6")
+      .set("version", 1)
+      .set("status_id", "e24a0cdf-c860-48f2-b597-689f165fd110")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "4f9bec7b-c99a-404b-9059-6f47356e1d7f")
+      .set("page_id", "187ff9e4-50fa-4824-a46a-3c40420942d3")
+      .set("version", 2)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "530837c1-1766-44c3-a234-2ae88e06d757")
+      .set("page_id", "187ff9e4-50fa-4824-a46a-3c40420942d3")
+      .set("version", 3)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "c108dbd0-880c-4684-8ae6-38a71e7ce175")
+      .set("page_id", "187ff9e4-50fa-4824-a46a-3c40420942d3")
+      .set("version", 4)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "1253a427-8aef-43c8-8d16-2a069d20fec4")
+      .set("page_id", "187ff9e4-50fa-4824-a46a-3c40420942d3")
+      .set("version", 5)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "8c927b7b-72c4-4252-a267-5f9ccc3322af")
+      .set("page_id", "187ff9e4-50fa-4824-a46a-3c40420942d3")
+      .set("version", 6)
+      .set("status_id", "b0389f50-4dc3-4be1-ad1e-070573a3f999")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "d29524a6-842f-46da-8982-afda2606e529")
+      .set("page_id", "f60cc515-1e27-4c98-a34a-adf327ac9c8d")
+      .set("version", 1)
+      .set("status_id", "e24a0cdf-c860-48f2-b597-689f165fd110")
+  );
+  
+  _db.insertIfNotExists(
+    "page_version",
+    _val.init()
+      .set("uid", "fe0924c4-3deb-498f-acb2-dfe76533deaf")
+      .set("page_id", "038dbda2-d275-488f-8042-fb20a6262d7c")
+      .set("version", 1)
+      .set("status_id", "e24a0cdf-c860-48f2-b597-689f165fd110")
+  );
+  
