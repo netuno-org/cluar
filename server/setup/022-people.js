67aff7864d53a4d4d60b2bd5af794a1e57cfdc40
jcardoso17 <jpcardoso97@gmail.com>
2025-05-28 17:44:24 +0100

    Update setup

---
+++ server/setup/022-people.js
@@ -0,0 +1,23 @@
+
+
+// -----------------------------------------------------------
+// 
+// PEOPLE
+// 
+// -----------------------------------------------------------
+// 
+// CODE GENERATED AUTOMATICALLY
+// 
+
+
+_db.insertIfNotExists(
+    "people",
+    _val.init()
+      .set("uid", "c8d507dc-e6b9-4850-92ef-14f710745573")
+      .set("name", "admin")
+      .set("people_user_id", 2)
+      .set("email", "admin@admin.com")
+      .set("avatar", "")
+      .set("recovery_key", "")
+);
+  
