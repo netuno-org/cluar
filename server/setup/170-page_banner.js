095574df8f77dc214a992d1b2d10d5d4eeccb643
joedson.souza <joedsonsouza.ifba.br@gmail.com>
2025-08-05 18:23:51 +0000

    update: default font for titles.

--- server/setup/170-page_banner.js
+++ server/setup/170-page_banner.js
@@ -17,12 +17,12 @@ _db.insertIfNotExists(
     .set("page_version_id", "9529dfcc-9a15-45ab-9f30-fee4bc75d154")
     .set("image", _storage.filesystem('server', 'default-banner-background.jpg').file())
     .set("image_title", "")
-    .set("title", '<p><span style="color: rgb(255, 255, 255); background-color: rgba(255, 255, 255, 0); font-size: 50px;">Prestes a Voar</span></p>')
+    .set("title", '<p><span style="color: rgb(255, 255, 255); background-color: rgba(255, 255, 255, 0); font-size: 50px; font-family: Arial;">Prestes a Voar</span></p>')
     .set("sorter", 10)
     .set("image_alt", "")
     .set("position_x", "")
     .set("position_y", "")
-    .set("content", '<p><span style="color: rgb(255, 255, 255); background-color: rgba(0, 0, 0, 0); font-size: 22px;">Aqui é onde você lança websites em uma outra dimensão.</span></p>')
+    .set("content", '<p><span style="color: rgb(255, 255, 255); background-color: rgba(0, 0, 0, 0); font-size: 22px; font-family: Arial;">Aqui é onde você lança websites em uma outra dimensão.</span></p>')
     .set("type_id", "d7ca6e7d-08a7-4ec0-8d16-9e3f09cd657c")
 );
 
@@ -34,11 +34,11 @@ _db.insertIfNotExists(
     .set("page_version_id", "eb14d6ee-db83-434c-879e-20e0825cff21")
     .set("image", _storage.filesystem('server', 'default-banner-background.jpg').file())
     .set("image_title", "")
-    .set("title", '<p><span style="color: rgb(255, 255, 255); background-color: rgba(255, 255, 255, 0); font-size: 50px;">Ready to Fly</span></p>')
+    .set("title", '<p><span style="color: rgb(255, 255, 255); background-color: rgba(255, 255, 255, 0); font-size: 50px; font-family: Arial;">Ready to Fly</span></p>')
     .set("sorter", 10)
     .set("image_alt", "")
     .set("position_x", "")
     .set("position_y", "")
-    .set("content", '<p><span style="color: rgb(255, 255, 255); background-color: rgba(0, 0, 0, 0); font-size: 22px;">Here is where you launch websites into another dimension.</span></p>')
+    .set("content", '<p><span style="color: rgb(255, 255, 255); background-color: rgba(0, 0, 0, 0); font-size: 22px; font-family: Arial;">Here is where you launch websites into another dimension.</span></p>')
     .set("type_id", "d7ca6e7d-08a7-4ec0-8d16-9e3f09cd657c")
 );
