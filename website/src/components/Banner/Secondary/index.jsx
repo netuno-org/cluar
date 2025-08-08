263078445693ca9eaedfe881d531005a2360e5f4
joedson.souza <joedsonsouza.ifba.br@gmail.com>
2025-08-08 14:55:42 +0000

    update: types of dynamic banner blocks.

---
+++ website/src/components/Banner/Secondary/index.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Row, Col } from "antd";
+
+import Actions from "../../Actions";
+import Cluar from "../../../common/Cluar";
+
+import "./index.less";
+
+function Secondary({
+  section,
+  type,
+  image,
+  image_title,
+  image_alt,
+  title,
+  content,
+  position,
+  actions,
+}) {
+  let backgroundPositionX = position.x !== "" ? position.x : "50%";
+  let backgroundPositionY = position.y !== "" ? position.y : "50%";
+
+  return (
+    <section className="banner">
+      <div
+        className={`banner__secondary`}
+        style={{
+          backgroundImage:
+            image?.indexOf("base64") === -1
+              ? `url(/cluar/images/page_${section}/${image})`
+              : `url(${image})`,
+          backgroundPositionX: backgroundPositionX,
+          backgroundPositionY: backgroundPositionY,
+        }}
+      >
+        <Row className="banner__wrapper" justify="center">
+          <Col lg={18} sm={24}>
+            <div>
+              <h1
+                data-sal="slide-down"
+                data-sal-duration="2000"
+                data-sal-easing="ease-out-cubic"
+                dangerouslySetInnerHTML={{ __html: Cluar.plainTitle(title) }}
+              />
+              <div
+                data-sal="fade"
+                data-sal-duration="2000"
+                data-sal-easing="ease-out-cubic"
+                dangerouslySetInnerHTML={{ __html: content }}
+              ></div>
+            </div>
+          </Col>
+          <Col lg={6} sm={24}>
+            <Actions {...{ section, type, actions }} />
+          </Col>
+        </Row>
+        <div className="banner__darken-bg"></div>
+      </div>
+    </section>
+  );
+}
+
+export default Secondary;
--- website/src/components/Banner/index.jsx
+++ website/src/components/Banner/index.jsx
@@ -1,66 +1,38 @@
 import React from "react";
-import { Row, Col } from "antd";
+import Default from "./Default";
+import Secondary from "./Secondary";
+import DefaultSubBanner from "./DefaultSubBanner";
 
-import Actions from "../Actions";
-import Cluar from "../../common/Cluar";
+const Banner = (props) => {
+    let layout = null;
 
-import "./index.less";
+    if (props.type === 'Default') {
+        layout = (
+            <Default
+                {...props}
+            />
+        );
+    } else if (props.type === 'Secondary') {
+        layout = (
+            <Secondary
+                {...props}
+            />
+        );
+    } else if (props.type === 'DefaultSubBanner') {
+        layout = (
+            <DefaultSubBanner
+                {...props}
+            />
+        );
+    } else {
+        layout = (
+            <Default
+                {...props}
+            />
+        );
+    }
 
-function Banner({
-  section,
-  type,
-  image,
-  image_title,
-  image_alt,
-  title,
-  content,
-  position,
-  actions,
-}) {
-  let backgroundPositionX = position.x !== "" ? position.x : "50%";
-  let backgroundPositionY = position.y !== "" ? position.y : "50%";
-
-  return (
-    <section className="banner">
-      <div
-        className={`banner__${type}`}
-        style={{
-          backgroundImage:
-            image?.indexOf("base64") === -1
-              ? `url(/cluar/images/page_${section}/${image})`
-              : `url(${image})`,
-          backgroundPositionX: backgroundPositionX,
-          backgroundPositionY: backgroundPositionY,
-        }}
-      >
-        <Row className="banner__wrapper" justify="center">
-          <Col lg={18} sm={24}>
-            <div>
-              <h1
-                data-sal="slide-down"
-                data-sal-duration="2000"
-                data-sal-easing="ease-out-cubic"
-                dangerouslySetInnerHTML={{ __html: Cluar.plainTitle(title) }}
-              />
-              <div
-                data-sal="fade"
-                data-sal-duration="2000"
-                data-sal-easing="ease-out-cubic"
-                dangerouslySetInnerHTML={{ __html: content }}
-              ></div>
-            </div>
-          </Col>
-          <Col lg={6} sm={24}>
-            <Actions {...{ section, type, actions }} />
-          </Col>
-        </Row>
-        <div className="banner__sub-banner">
-          {Cluar.plainDictionary("text-sub-banner")}
-        </div>
-        <div className="banner__darken-bg"></div>
-      </div>
-    </section>
-  );
+    return <section className="banner">{layout}</section>
 }
 
 export default Banner;
