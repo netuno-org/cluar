5732cbf4cbd110c3cd9e82e432021f451977f056
joedson.souza <joedsonsouza.ifba.br@gmail.com>
2025-08-14 18:50:14 +0000

    new: service to edit an action.

---
+++ server/services/actions/put.js
@@ -0,0 +1,47 @@
+const uid = _req.getString("uid");
+
+const languageCode = _req.getString("language_code");
+const title = _req.getString("title");
+const content = _req.getString("content");
+const indication = _req.getString("indication");
+const link = _req.getString("link");
+const active = _req.getBoolean("active");
+// const image = _req.getFile("image");
+
+const dbLanguage = _db.queryFirst(`
+    SELECT id, code, description FROM language WHERE code = ?
+`, languageCode);
+
+const dbAction = _db.get('action', uid);
+if (!dbAction) {
+    _header.status(404);
+    _out.json(
+        _val
+            .map()
+            .set("result", false)
+            .set("error", `action not found with uid: ${uid}`)
+            .set("error_code", `action-not-found`)
+    );
+    _exec.stop();
+}
+
+const data = _val.map()
+    .set('title', title)
+    .set('content', content)
+    .set('indication', indication)
+    .set('link', link)
+    .set('active', active)
+    .set("language_id", dbLanguage.getInt("id"));
+// .set("image", image)
+
+_db.update(
+    'action',
+    dbAction.getInt("id"),
+    data
+)
+
+
+_out.json(
+    _val.map()
+        .set('result', true)
+);
