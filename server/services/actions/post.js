7d294ffccdcb2bf6b72e68928c28276c7a5e5226
joedson.souza <joedsonsouza.ifba.br@gmail.com>
2025-08-14 18:17:17 +0000

    new: service to create new action.

---
+++ server/services/actions/post.js
@@ -0,0 +1,43 @@
+// _core: db/insertAndReturn
+
+const languageCode = _req.getString("language_code");
+
+const title = _req.getString("title");
+const content = _req.getString("content");
+const indication = _req.getString("indication");
+const link = _req.getString("link");
+const active = _req.getBoolean("active");
+// const image = _req.getFile("image");
+
+const dbLanguage = _db.queryFirst(`
+    SELECT id, code, description FROM language WHERE code = ?
+`, languageCode);
+
+if (!dbLanguage) {
+    _header.status(404);
+    _out.json(
+        _val.map()
+            .set('result', false)
+            .set('error', `language not found with code: ${languageCode}`)
+            .set('error_code', `language-not-found`)
+    );
+    _exec.stop();
+}
+
+
+const data = _val.map()
+    .set('title', title)
+    .set('content', content)
+    .set('indication', indication)
+    .set('link', link)
+    .set('active', active)
+    .set("language_id", dbLanguage.getInt("id"))
+    // .set("image", image)
+const dbAction = insertAndReturn('action', data);
+
+_log.info("dbAction:", dbAction);
+
+_out.json(
+    _val.map()
+        .set('result', true)
+);
