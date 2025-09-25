ed8d261988310f514ee1388366f581326e55172e
Eduardo Velasques <eduveks@gmail.com>
2025-09-25 20:52:52 +0100

    Website Vite configuration was been upgraded.

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
-import vitePluginImp from 'vite-plugin-imp';
-import react from '@vitejs/plugin-react-swc';
-import styleX from "vite-plugin-stylex";
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
     strictPort: true
   },
   plugins: [
-    react(),
-    styleX()
-  ]
+    react()
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true
+      },
+    },
+  }
 })
