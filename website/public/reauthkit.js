5a78bcec2802556f2a36a6de9695dae276dab44a
Eduardo Velasques <eduveks@gmail.com>
2026-01-08 12:34:43 +0000

    Website public config js removed.

--- website/public/reauthkit.js
+++
@@ -1,9 +0,0 @@
-window.reauthkit = { config: {
-    "api": {},
-    "auth": {"providers": {
-        "discord": false,
-        "facebook": false,
-        "github": false,
-        "google": false
-    }}
-} };
