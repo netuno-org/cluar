92eaf3c3fcd06365b1cd92249cac174b6ec9a3ac
joedson souza <joedson.souza@sitana.pt>
2026-01-16 13:00:44 -0300

    new: modal for slider item content.

--- website/src/base/PageSection/SliderEditor/SortableSliderItem/index.jsx
+++ website/src/base/PageSection/SliderEditor/SortableSliderItem/index.jsx
@@ -1,4 +1,5 @@
-import { Col, Form, Input, Collapse, Button, Flex, Select } from "antd";
+import React, { useEffect, useState } from "react";
+import { Col, Form, Input, Collapse, Button, Flex, Select, Modal, Card, message } from "antd";
 
 import {
   DndContext,
@@ -15,7 +16,7 @@ import {
 } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
-import { HolderOutlined, CloseOutlined } from "@ant-design/icons";
+import { HolderOutlined, CloseOutlined, EditOutlined } from "@ant-design/icons";
 import ImageSectionEditor from "../../ImageSectionEditor";
 import LexicalEditor from "../../../LexicalEditor";
 import Cluar from "../../../../common/Cluar";
@@ -39,129 +40,185 @@ const SortableItem = ({
     transition,
   };
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [contentValue, setContentValue] = useState(item?.content || "");
+
+  useEffect(() => {
+    setContentValue(item?.content || "");
+  }, [item]);
+
+  const handleSaveModal = () => {
+    form.setFieldsValue({
+      itemsByUid: {
+        [itemIndex]: {
+          content: contentValue
+        }
+      }
+    });
+
+    onChangeItem(item.uid, "content", contentValue);
+
+    setIsModalOpen(false);
+    message.success("Conteúdo do item atualizado!");
+  };
+
   return (
-    <Col
-      span={24}
-      key={item.uid}
-      style={style}
-      className="sortable-slider-item"
-    >
-      <Flex align="center" className="sortable-slider-item__wrapper">
-        <Collapse
-          className="sortable-slider-item__wrapper__collapse"
-          destroyInactivePanel={false}
-          items={[
-            {
-              key: itemIndex,
-              label: Cluar.plainHTML(item?.title),
-              extra: (
-                <HolderOutlined
-                  ref={setNodeRef}
-                  {...attributes}
-                  {...listeners}
-                  onClick={(e) => e.stopPropagation()}
-                  style={{ cursor: "grab" }}
-                />
-              ),
-              children: (
-                <div>
-                  <Form.Item label="Título" style={{ marginBottom: 8 }}>
-                    <LexicalEditor
-                      key={`${item.uid}-title`}
-                      initialHtml={item?.title}
-                      onChange={(html) => onChangeItem(item.uid, "title", html)}
-                      mode="simple"
-                    />
-                  </Form.Item>
-
-                  <Form.Item name={["itemsByUid", itemIndex, "title"]} hidden>
-                    <Input
-                      onChange={(e) =>
-                        onChangeItem(item.uid, "title", e.target.value)
-                      }
-                    />
-                  </Form.Item>
-
-                  <Form.Item label="Conteúdo" style={{ marginBottom: 8 }}>
-                    <LexicalEditor
-                      key={`${item.uid}-content`}
-                      initialHtml={item?.content}
-                      onChange={(html) => onChangeItem(item.uid, "content", html)}
-                    />
-                  </Form.Item>
-
-                  <Form.Item name={["itemsByUid", itemIndex, "content"]} hidden>
-                    <Input
-                      onChange={(e) =>
-                        onChangeItem(item.uid, "content", e.target.value)
-                      }
-                    />
-                  </Form.Item>
-
-                  {showActions && (
-                    <Form.Item label="Actions" name={["itemsByUid", itemIndex, "action_uids"]}>
-                      <Select
-                        options={actionsData.map((action) => ({
-                          label: action.title,
-                          value: action.uid,
-                        }))}
-                        onChange={(val) =>
-                          onChangeItem(item.uid, "action_uids", val)
-                        }
-                        placeholder="Adicionar"
-                        mode="multiple"
-                        allowClear
+    <>
+      <Col
+        span={24}
+        key={item.uid}
+        style={style}
+        className="sortable-slider-item"
+      >
+        <Flex align="center" className="sortable-slider-item__wrapper">
+          <Collapse
+            className="sortable-slider-item__wrapper__collapse"
+            destroyInactivePanel={false}
+            items={[
+              {
+                key: itemIndex,
+                label: Cluar.plainHTML(item?.title),
+                extra: (
+                  <HolderOutlined
+                    ref={setNodeRef}
+                    {...attributes}
+                    {...listeners}
+                    onClick={(e) => e.stopPropagation()}
+                    style={{ cursor: "grab" }}
+                  />
+                ),
+                children: (
+                  <div>
+                    <Form.Item label="Título" style={{ marginBottom: 8 }}>
+                      <LexicalEditor
+                        key={`${item.uid}-title`}
+                        initialHtml={item?.title}
+                        onChange={(html) => onChangeItem(item.uid, "title", html)}
+                        mode="simple"
                       />
                     </Form.Item>
-                  )}
-                  <ImageSectionEditor
-                    sectionData={item}
-                    form={form}
-                    imageName={["itemsByUid", itemIndex, "image"]}
-                    imageTitleName={["itemsByUid", itemIndex, "image_title"]}
-                    imageAltName={["itemsByUid", itemIndex, "image_alt"]}
-                    onChangeImage={(val) =>
-                      onChangeItem(item.uid, "image", val)
-                    }
-                    onChangeImageAlt={(val) =>
-                      onChangeItem(item.uid, "image_alt", val)
-                    }
-                    onChangeImageTitle={(val) =>
-                      onChangeItem(item.uid, "image_title", val)
-                    }
-                  />
-                  <Form.Item
-                    label="Section"
-                    name={["itemsByUid", itemIndex, "section"]}
-                    initialValue={item.section}
-                    style={{ display: "none" }}
-                  >
-                    <Input rows={3} />
-                  </Form.Item>
-                  <Form.Item
-                    label="UID"
-                    name={["itemsByUid", itemIndex, "uid"]}
-                    initialValue={item.uid}
-                    style={{ display: "none" }}
-                  >
-                    <Input rows={3} />
-                  </Form.Item>
-                </div>
-              ),
-            },
-          ]}
-        />
-        <Button
-          type="text"
-          onClick={(e) => {
-            e.stopPropagation();
-            onRemoveItem(item.uid);
-          }}
-        >
-          <CloseOutlined />
-        </Button>
-      </Flex>
-    </Col>
+
+                    <Form.Item name={["itemsByUid", itemIndex, "title"]} hidden>
+                      <Input
+                        onChange={(e) =>
+                          onChangeItem(item.uid, "title", e.target.value)
+                        }
+                      />
+                    </Form.Item>
+
+                    <Form.Item label="Conteúdo">
+                      <Card
+                        size="small"
+                        actions={[
+                          <div style={{ textAlign: 'left', paddingLeft: '12px' }}>
+                            <Button
+                              type="primary"
+                              icon={<EditOutlined />}
+                              onClick={() => setIsModalOpen(true)}
+                            >
+                              Editar Conteúdo
+                            </Button>
+                          </div>
+                        ]}
+                      >
+                        <div>
+                          {Cluar.plainHTML(contentValue).slice(0, 97) + "..."}
+                        </div>
+                      </Card>
+                    </Form.Item>
+
+                    <Form.Item name={["itemsByUid", itemIndex, "content"]} hidden>
+                      <Input
+                        onChange={(e) =>
+                          onChangeItem(item.uid, "content", e.target.value)
+                        }
+                      />
+                    </Form.Item>
+
+                    {showActions && (
+                      <Form.Item label="Actions" name={["itemsByUid", itemIndex, "action_uids"]}>
+                        <Select
+                          options={actionsData.map((action) => ({
+                            label: action.title,
+                            value: action.uid,
+                          }))}
+                          onChange={(val) =>
+                            onChangeItem(item.uid, "action_uids", val)
+                          }
+                          placeholder="Adicionar"
+                          mode="multiple"
+                          allowClear
+                        />
+                      </Form.Item>
+                    )}
+                    <ImageSectionEditor
+                      sectionData={item}
+                      form={form}
+                      imageName={["itemsByUid", itemIndex, "image"]}
+                      imageTitleName={["itemsByUid", itemIndex, "image_title"]}
+                      imageAltName={["itemsByUid", itemIndex, "image_alt"]}
+                      onChangeImage={(val) =>
+                        onChangeItem(item.uid, "image", val)
+                      }
+                      onChangeImageAlt={(val) =>
+                        onChangeItem(item.uid, "image_alt", val)
+                      }
+                      onChangeImageTitle={(val) =>
+                        onChangeItem(item.uid, "image_title", val)
+                      }
+                    />
+                    <Form.Item
+                      label="Section"
+                      name={["itemsByUid", itemIndex, "section"]}
+                      initialValue={item.section}
+                      style={{ display: "none" }}
+                    >
+                      <Input rows={3} />
+                    </Form.Item>
+                    <Form.Item
+                      label="UID"
+                      name={["itemsByUid", itemIndex, "uid"]}
+                      initialValue={item.uid}
+                      style={{ display: "none" }}
+                    >
+                      <Input rows={3} />
+                    </Form.Item>
+                  </div>
+                ),
+              },
+            ]}
+          />
+          <Button
+            type="text"
+            onClick={(e) => {
+              e.stopPropagation();
+              onRemoveItem(item.uid);
+            }}
+          >
+            <CloseOutlined />
+          </Button>
+        </Flex>
+      </Col>
+
+      <Modal
+        title="Editar Conteúdo"
+        open={isModalOpen}
+        onOk={handleSaveModal}
+        onCancel={() => setIsModalOpen(false)}
+        width={1000}
+        okText="Salvar"
+        cancelText="Cancelar"
+        centered
+        destroyOnClose
+      >
+        <div>
+          <LexicalEditor
+            initialHtml={contentValue}
+            onChange={(html) => setContentValue(html)}
+          />
+        </div>
+      </Modal>
+    </>
   );
 };
 
