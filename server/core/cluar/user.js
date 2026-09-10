9274ccdc8b4149f7f12ac50458efb6f065ec6b41
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-09-10 16:44:01 +0000

    refactor: more descriptive name for the function

--- server/core/cluar/user.js
+++ server/core/cluar/user.js
@@ -54,7 +54,7 @@ export default {
     return dbOrganizations;
   },
 
-  getOrganizationHierarchy: () => {
+  getAuthorizedOrganizations: () => {
     const dbPeople = _db.queryFirst(`SELECT id FROM people WHERE people_user_id = ?`, _user.id());
 
     const dbOrganizations = _db.query(`
