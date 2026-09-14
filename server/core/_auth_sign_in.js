1fbbf34b473f452a5b6ef38e765a0037f1b2167c
anupamme <mediratta@gmail.com>
2026-09-14 23:46:12 +0000

    harden: add parameterized queries in _auth_sign_in.js
    SQL query constructed using JavaScript template literals with dynamic input
    Addresses utils.custom.sql-injection-template-literal

--- server/core/_auth_sign_in.js
+++ server/core/_auth_sign_in.js
@@ -21,14 +21,14 @@ const isAuthorized = _db.queryFirst(`
     SELECT 1
     FROM organization_people
     WHERE 1 = 1
-        AND people_id = ${dbPeople.getInt("id")}
+        AND people_id = ${_db.param("int")}
         AND user_group_id IN (
             SELECT id FROM user_group WHERE code IN (
-                ${authorizedGroups.map((group) => `'${group}'`).join(", ")}
+                ${authorizedGroups.map(() => "?").join(", ")}
             )
         )
         AND active = true
-`);
+`, dbPeople.getInt("id"), ...authorizedGroups);
 
 if (!isAuthorized) {
   _auth.signInAbortWithData(
