7f27b8fe5c471114add49082ebd6727ac2175d98
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-09-21 20:16:29 +0000

    feat: permissions and error handling for user/delete

--- server/services/reserved-area/user/delete.js
+++ server/services/reserved-area/user/delete.js
@@ -1,4 +1,5 @@
 import { _db, _val, _out, _exec, _user, _req } from "@netuno/server-types";
+import cluar from "#core/cluar/main.js"
 
 const profileUid = _req.getString("uid");
 
@@ -6,25 +7,48 @@ const dbProfile = _db.queryFirst(`
     SELECT * FROM profile WHERE uid = ?::uuid 
 `, profileUid);
 
-// existe alguma organização à qual o usuário a ser deletado pertença
-// ou organização acima dessa
-// da qual o usuário logado seja administrador?
-if (dbProfile) {
-  const profileId = dbProfile.getInt("id")
-  _db.execute(`DELETE from organization_profile WHERE profile_id = ${profileId}`);
-  _db.delete(
-    "profile",
-    profileId
-  );
-  _user.remove(dbProfile.getInt("profile_user_id"));
-  _out.json(
-    _val.map()
-      .set("result", true)
-  );
-} else {
-  _out.output(404);
-  _out.json(
-    _val.map()
-      .set("error", "not-exist")
-  );
+const loggedUserUid = cluar.user.getProfile().getString("uid");
+if (profileUid === loggedUserUid) {
+  cluar.response.error({ status: 409, error: "users cannot delete themselves" });
 }
+
+if (!dbProfile) {
+  cluar.response.error({ status: 404, error: "user not found" });
+}
+
+// o usuário só pode ser deletado se ele perterncer à apenas uma organização
+// essa organização deve estar na árvore de organizações do usuário logado
+const profileId = dbProfile.getInt("id");
+const membership = _db.queryFirst(`
+    SELECT
+        count(*) over(),
+        organization.id as user_organization_id
+    FROM organization_profile
+    INNER JOIN organization
+    ON organization_profile.organization_id = organization.id
+    WHERE profile_id = ${_db.param("int")} 
+`, profileId);
+
+if (membership.getInt("count") > 1) {
+  cluar.response.error({
+    status: 409,
+    error: 'Cannot remove user, they belong to more than one organization.'
+  });
+}
+
+const userOrganizationId = membership.getInt("user_organization_id");
+const loggedUserOganizations = cluar.user.getActiveAdminOrganizationsWithDescendants();
+if (!loggedUserOganizations.some((org) => org.getInt("id") === userOrganizationId)) {
+  cluar.response.error({ status: 403, error: 'permission denied' });
+}
+
+_db.execute(`DELETE from organization_profile WHERE profile_id = ${profileId}`);
+_db.delete(
+  "profile",
+  profileId
+);
+_user.remove(dbProfile.getInt("profile_user_id"));
+_out.json(
+  _val.map()
+    .set("result", true)
+);
