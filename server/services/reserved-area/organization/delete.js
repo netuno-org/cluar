ee6856e8409af4babe4d24e691cddfab139e2b74
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-09-21 19:02:01 +0000

    feat: delete org and it's children but only if none has members

--- server/services/reserved-area/organization/delete.js
+++ server/services/reserved-area/organization/delete.js
@@ -16,29 +16,46 @@ if (!userOganizations.some((org) => org.getString("uid") === organizationUid)) {
 
 const organizationId = dbOrganization.getInt("id");
 
-const dbChildOrganizations = _db.query(`
-    SELECT * FROM organization
-    WHERE parent_id = ?::int
+const dbOrganizationAndChildren = _db.query(`
+    WITH RECURSIVE org_and_children AS (
+        SELECT id, uid, active, code, name, parent_id
+        FROM organization
+        WHERE id = ?::int
+
+        UNION
+
+        SELECT org.id, org.uid, org.active, org.code, org.name, org.parent_id
+        FROM organization org
+        INNER JOIN org_and_children oac
+        ON org.parent_id = oac.id
+    )
+    SELECT *
+    FROM org_and_children;
   `, organizationId
 );
 
-for (const dbChildOrganization of dbChildOrganizations) {
-  const childOrganizationId = dbChildOrganization.getInt("id");
+for (const dbOrgOrChild of dbOrganizationAndChildren) {
+  const orgOrChildId = dbOrgOrChild.getInt("id");
+  const membership = _db.form("organization_profile")
+    .where(_db.where("organization_id").equals(orgOrChildId));
+
+  if (membership.count() >= 1) {
+    cluar.response.error({
+      status: 409,
+      error: `there are members in this organization or one of it's children, cannot remove it.`
+    });
+  }
+}
+
+for (const dbOrgOrChild of dbOrganizationAndChildren) {
+  const orgOrChildId = dbOrgOrChild.getInt("id");
   _db.execute(`
       DELETE FROM organization_profile
       WHERE organization_id = ?::int
-    `, childOrganizationId
+    `, orgOrChildId
   );
 
-  _db.execute(`DELETE FROM organization WHERE id = ?::int`, childOrganizationId);
+  _db.execute(`DELETE FROM organization WHERE id = ?::int`, orgOrChildId);
 }
-
-_db.execute(`
-    DELETE FROM organization_profile
-    WHERE organization_id = ?::int
-  `, organizationId
-);
-
-_db.execute(`DELETE FROM organization WHERE uid = ?::uuid`, organizationUid);
 
 _out.json(_val.map().set("result", true));
