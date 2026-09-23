97d786818932c0b52b75c150fa19bdbeba560190
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-09-23 14:30:19 +0000

    test: delete user without begin admin of the org

--- server/__tests__/user/delete.test.js
+++ server/__tests__/user/delete.test.js
@@ -69,3 +69,15 @@ it("shouldn't delete a user if they are in more than one organization", async ()
 
   await removeUserFromOrganization(charlieUid, bOrgUid);
 });
+
+it("shouldn't delete a user if logged user is not in the admin group of the user's organization", async () => {
+  await addUserToOrganization(aliceUid, "b", "editor");
+
+  const accessToken = await login.asAlice();
+  await request(NETUNO_URL)
+    .delete(`/reserved-area/user?uid=${bobUid}`)
+    .set("Authorization", `Bearer ${accessToken}`)
+    .expect(403);
+
+  await removeUserFromOrganization(aliceUid, bOrgUid);
+});
