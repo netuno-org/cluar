5f9a25c2fc53a67f26945cccfe20a260b385dddd
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-09-23 16:00:54 +0000

    refactor: add optional error_code argument to cluar.response.error()

--- server/core/cluar/response.js
+++ server/core/cluar/response.js
@@ -1,12 +1,13 @@
 import { _exec, _header, _out, _val } from "@netuno/server-types";
 
 export default {
-  error: ({ status, error }) => {
+  error: ({ status, error, error_code }) => {
     _header.status(status);
     _out.json(
       _val.map()
         .set("result", false)
         .set("error", error)
+        .set("error_code", error_code)
     );
     _exec.stop();
   },
