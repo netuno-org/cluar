efb27c416656ff9e6aa0a5c929b81c27efc95816
Eduardo Velasques <eduveks@gmail.com>
2026-09-25 18:42:37 +0100

    Update _start.js

--- server/setup/_start.js
+++ server/setup/_start.js
@@ -1,5 +1,5 @@
 import { _db, _val } from "@netuno/server-types";
 
-if (_db.query("SELECT * FROM page").size() == 0) {
+if (_db.checkExists().table("page") && _db.query("SELECT * FROM page").size() == 0) {
   _val.global().set("cluar:setup", true);
 }
